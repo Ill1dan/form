b4a20d421de781e086d5ec731fde8962b0644294
Abdul Ahad <147724049+Ill1dan@users.noreply.github.com>
2023-10-24 20:33:51 +0600

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -13,14 +13,6 @@ $to = 'abdulahadkashem@gmail.com';
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 
-if (mail($to, $email_subject, $email_body, $headers)) {
-            header("Location: index.html");
-            exit();
-        } else {
-            echo "Email could not be sent. Please try again later.";
-        }
-    } else {
-        echo "Invalid email address. Please provide a valid email.";
-    }
-}
+mail($to,$email_subject,$email_body,$headers);
+header("Location: index.html");
 ?>
