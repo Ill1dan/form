0725b10093b5e329dd644aa139aeabdbb830ab93
Abdul Ahad <147724049+Ill1dan@users.noreply.github.com>
2023-10-24 19:47:45 +0600

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,20 +1,26 @@
 <?php
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
-$file = $_POST['file'];
 $message = $_POST['message'];
 
 $email_from = 'random@people.com';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
-				"User Email: $visitor_email.\n".
-				"User file: $file.\n".
-				"User Message: $message.\n".
+		"User Email: $visitor_email.\n".
+		"User Message: $message.\n";
 $to = 'abdulahadkashem@gmail.com';
 
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
-header("Location: index.html");
+if (mail($to, $email_subject, $email_body, $headers)) {
+            header("Location: index.html");
+            exit();
+        } else {
+            echo "Email could not be sent. Please try again later.";
+        }
+    } else {
+        echo "Invalid email address. Please provide a valid email.";
+    }
+}
 ?>
